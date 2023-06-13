433bfdac3ad807966944e9515ba932ce4d532ee7
zeroling147 <tangjize2022liu@outlook.com>
2023-06-13 22:34:48 +0800

    I am zeroling. This my first commit!!! :D

---
+++ Hello/Hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * 这个东西叫注释，可以在里面放各种说明文字！
+ * 或者日期：2023-6-13
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello, I am ling!" << std::endl;
+	return 0;
+}
+
+// zhe ge ye shi zhu shi
+// haode, cheng gong le!!!!!
+// an Ctrl S ye ke yi bao cun.
